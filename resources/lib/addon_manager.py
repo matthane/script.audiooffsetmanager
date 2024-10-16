b85092f0a9efd7e2345892fb038b2cbea64e450d
matthane <matthan.evans@icloud.com>
2024-10-16 09:36:38 -0500

    Move initialization to addon_manager

---
+++ resources/lib/addon_manager.py
@@ -0,0 +1,41 @@
+import xbmc
+from resources.lib.stream_info import StreamInfo
+from resources.lib.event_manager import EventManager
+from resources.lib.settings_manager import SettingsManager
+from resources.lib.offset_manager import OffsetManager
+from resources.lib.seek_backs import SeekBacks
+
+class AddonManager:
+    def __init__(self):
+        # Initialize the event manager
+        self.event_manager = EventManager()
+
+        # Initialize the settings manager
+        self.settings_manager = SettingsManager()
+
+        # Initialize stream info module
+        self.stream_info = StreamInfo(self.event_manager)
+
+        # Initialize offset manager
+        self.offset_manager = OffsetManager(self.event_manager, self.stream_info)
+
+        # Initialize seek backs
+        self.seek_backs = SeekBacks(self.event_manager)
+
+    def start(self):
+        # Start all components
+        self.stream_info.start()
+        self.offset_manager.start()
+        self.seek_backs.start()
+
+    def stop(self):
+        # Stop all components
+        self.stream_info.stop()
+        self.offset_manager.stop()
+        self.seek_backs.stop()
+
+# Usage example:
+# addon_manager = AddonManager()
+# addon_manager.start()
+# xbmc.Monitor().waitForAbort()
+# addon_manager.stop()
