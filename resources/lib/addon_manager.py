7a490b82e57299a8d81dc39c4ff14876fff475fc
matthane <matthan.evans@icloud.com>
2024-10-17 16:04:03 -0500

    Update to init active monitor module

--- resources/lib/addon_manager.py
+++ resources/lib/addon_manager.py
@@ -6,6 +6,7 @@ from resources.lib.event_manager import EventManager
 from resources.lib.settings_manager import SettingsManager
 from resources.lib.offset_manager import OffsetManager
 from resources.lib.seek_backs import SeekBacks
+from resources.lib.active_monitor import ActiveMonitor
 
 
 class AddonManager:
@@ -25,17 +26,22 @@ class AddonManager:
         # Initialize seek backs
         self.seek_backs = SeekBacks(self.event_manager)
 
+        # Initialize active monitor
+        self.active_monitor = ActiveMonitor(self.event_manager, self.stream_info, self.settings_manager, self.offset_manager)
+
     def start(self):
         # Start all components
         self.stream_info.start()
         self.offset_manager.start()
         self.seek_backs.start()
+        self.active_monitor.start()
 
     def stop(self):
         # Stop all components
         self.stream_info.stop()
         self.offset_manager.stop()
         self.seek_backs.stop()
+        self.active_monitor.stop()
 
 
 # Usage example:
