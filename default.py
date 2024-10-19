da4049a5c376065bfb431a7ba3be414151050010
matthane <matthan.evans@icloud.com>
2024-10-18 22:46:28 -0500

    Update for onboard script process

--- default.py
+++ default.py
@@ -1,12 +1,20 @@
 import xbmc
+import sys
 from resources.lib.addon_manager import AddonManager
 
-# Initialize and start the addon manager
+# Initialize the addon manager
 addon_manager = AddonManager()
-addon_manager.start()
 
-# Keep the script running
-xbmc.Monitor().waitForAbort()
+# Check if we're being called with arguments
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'play_test_video':
+        addon_manager.play_test_video()
+else:
+    # Start the addon manager for normal operation
+    addon_manager.start()
 
-# On script exit, stop the addon manager
-addon_manager.stop()
+    # Keep the script running
+    xbmc.Monitor().waitForAbort()
+
+    # On script exit, stop the addon manager
+    addon_manager.stop()
