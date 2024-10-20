1436722dd1d22cae89f5f5efabaf7e0c076b007f
matthane <matthan.evans@icloud.com>
2024-10-20 17:30:23 -0500

    Break out test video script into separate script, reduced size of test video

--- default.py
+++ default.py
@@ -1,6 +1,5 @@
 # default.py
 
-import sys
 import xbmc
 import xbmcaddon
 from resources.lib.addon_manager import AddonManager
@@ -13,13 +12,10 @@ def main():
     addon_manager = AddonManager()
     addon_manager.start()
 
-    if len(sys.argv) > 1 and sys.argv[1] == 'play_test_video':
-        addon_manager.play_test_video()
-    else:
-        monitor = xbmc.Monitor()
-        while not monitor.abortRequested():
-            if monitor.waitForAbort(10):
-                break
+    monitor = xbmc.Monitor()
+    while not monitor.abortRequested():
+        if monitor.waitForAbort(10):
+            break
 
     addon_manager.stop()
 
